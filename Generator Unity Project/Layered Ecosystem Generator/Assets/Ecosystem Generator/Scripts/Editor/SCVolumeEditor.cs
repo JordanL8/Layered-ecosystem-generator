c4c959a4b8cd9110fe22368c85db0a7445d36576
Lewis Jordan <lewis.jordan@hotmail.co.uk>
2020-02-26 00:33:41 +0000

    Began editor tool for creating SC tree leaf volumes

---
+++ Generator Unity Project/Layered Ecosystem Generator/Assets/Ecosystem Generator/Scripts/Editor/SCVolumeEditor.cs
@@ -0,0 +1,341 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+// Code based on the following tutorial series by Sebastian Lague: https://www.youtube.com/watch?v=bPO7_JNWNmI
+
+[CustomEditor(typeof(SCVolume))]
+public class SCVolumeEditor : Editor
+{
+    public class SelectionInformation
+    {
+        public int m_shapeIndex;
+        public int m_mouseOverShapeIndex;
+
+        public int m_pointIndex = -1;
+        public bool m_mouseOverPoint;
+        public bool m_pointSelected;
+        public Vector3 m_dragStartPosition;
+
+        public int m_lineIndex = -1;
+        public bool m_mouseOverLine;
+    }
+
+
+    private SCVolume m_scVolume;
+    private SelectionInformation m_selectionInformation;
+    private SCVolumeShape SelectedShape { get { return m_scVolume?.m_volumeShapes[m_selectionInformation.m_shapeIndex]; } }
+
+    private float m_gizmoDiscRadius = 0.15f;
+    private float m_sqrGizmoDiscRadius;
+
+    private bool m_needsRepaint;
+
+    private void OnEnable()
+    {
+        SetInitialReferences();
+        Undo.undoRedoPerformed += OnUndoRedoPerformed;
+    }
+
+    private void OnDisable()
+    {
+        Undo.undoRedoPerformed -= OnUndoRedoPerformed;
+    }
+
+    private void SetInitialReferences()
+    {
+        m_scVolume = target as SCVolume;
+        m_selectionInformation = new SelectionInformation();
+        m_sqrGizmoDiscRadius = m_gizmoDiscRadius * m_gizmoDiscRadius;
+    }
+
+    private void OnSceneGUI()
+    {
+        Event guiEvent = Event.current;
+        HandleEvent(guiEvent);
+    }
+
+    private void HandleEvent(Event guiEvent)
+    {
+        if (guiEvent.type == EventType.Repaint)
+        {
+            DrawBounds();
+        }
+        else if (guiEvent.type == EventType.Layout)
+        {
+            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+        }
+        else
+        {
+            HandleInput(guiEvent);
+            if (m_needsRepaint)
+            {
+                HandleUtility.Repaint();
+                m_needsRepaint = false;
+            }
+        }
+    }
+
+    void OnUndoRedoPerformed()
+    {
+        if(m_selectionInformation.m_shapeIndex >= m_scVolume.m_volumeShapes.Count)
+        {
+            m_selectionInformation.m_shapeIndex = m_scVolume.m_volumeShapes.Count - 1;
+        }
+    }
+
+    #region Input
+
+    private void HandleInput(Event guiEvent)
+    {
+        Ray mouseRay = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition);
+        float drawPlaneDistance = 0;
+        float distanceToDrawPlane = (drawPlaneDistance - mouseRay.origin.z) / mouseRay.direction.z;
+        Vector3 mousePosition = mouseRay.GetPoint(distanceToDrawPlane);
+
+        if (guiEvent.button != 0)
+        {
+            return;
+        }
+
+        if (guiEvent.modifiers == EventModifiers.None)
+        {
+            if (guiEvent.type == EventType.MouseDown)
+            {
+                HandleLeftMouseDown(mousePosition);
+            }
+
+            if (guiEvent.type == EventType.MouseDrag)
+            {
+                HandleLeftMouseDrag(mousePosition);
+            }
+        }
+        else if (guiEvent.modifiers == EventModifiers.Control)
+        {
+            if (guiEvent.type == EventType.MouseDown)
+            {
+                HandleControlMouseDown(mousePosition);
+            }
+        }
+
+        if (guiEvent.type == EventType.MouseUp)
+        {
+            HandleLeftMouseUp(mousePosition);
+        }
+
+        if (!m_selectionInformation.m_pointSelected)
+        {
+            UpdateMouseOverInformation(mousePosition);
+        }
+    }
+
+    private void HandleControlMouseDown(Vector3 mousePosition)
+    {
+        CreateNewVolumeShape();
+        CreateNewVolumeShapePoint(mousePosition);
+    }
+
+    private void HandleLeftMouseDown(Vector3 mousePosition)
+    {
+        if(m_scVolume.m_volumeShapes.Count == 0)
+        {
+            CreateNewVolumeShape();
+        }
+
+        SelectShapeUnderMouse();
+
+        if (m_selectionInformation.m_mouseOverPoint)
+        {
+            SelectPointUnderMouse();
+        }
+        else
+        {
+            CreateNewVolumeShapePoint(mousePosition);
+        }
+    }
+
+    private void HandleLeftMouseUp(Vector3 mousePosition)
+    {
+        if(m_selectionInformation.m_pointSelected)
+        {
+            SelectedShape.m_boundingPoints[m_selectionInformation.m_pointIndex] = m_selectionInformation.m_dragStartPosition;
+            Undo.RecordObject(m_scVolume, "Move Point");
+            SelectedShape.m_boundingPoints[m_selectionInformation.m_pointIndex] = mousePosition;
+            m_selectionInformation.m_pointSelected = false;
+            m_selectionInformation.m_pointIndex = -1;
+            m_needsRepaint = true;
+        }
+    }
+
+    private void HandleLeftMouseDrag(Vector3 mousePosition)
+    {
+        if (m_selectionInformation.m_pointSelected)
+        {
+            SelectedShape.m_boundingPoints[m_selectionInformation.m_pointIndex] = mousePosition;
+            m_needsRepaint = true;
+        }
+    }
+
+    private void UpdateMouseOverInformation(Vector3 mousePosition)
+    {
+        // Check to see if the mouse is above any shape point.
+        int mouseOverPointIndex = -1;
+        int mouseOverShapeIndex = -1;
+        for (int i = 0; i < m_scVolume.m_volumeShapes.Count; i++)
+        {
+            SCVolumeShape curShape = m_scVolume.m_volumeShapes[i];
+            for (int j = 0; j < curShape.m_boundingPoints.Count; j++)
+            {
+                if (Vector3.SqrMagnitude(curShape.m_boundingPoints[j] - mousePosition) < m_sqrGizmoDiscRadius)
+                {
+                    mouseOverPointIndex = j;
+                    mouseOverShapeIndex = i;
+                    break;
+                }
+            }
+        }
+
+        // Update selected point information.
+        if (mouseOverPointIndex != m_selectionInformation.m_pointIndex || mouseOverShapeIndex != m_selectionInformation.m_mouseOverShapeIndex)
+        {
+            m_selectionInformation.m_mouseOverShapeIndex = mouseOverShapeIndex;
+
+            m_selectionInformation.m_pointIndex = mouseOverPointIndex;
+            m_selectionInformation.m_mouseOverPoint = mouseOverPointIndex != -1;
+
+            m_needsRepaint = true;
+        }
+
+        // Update selected line information based on the above point information. Points take preference over lines.
+        if (m_selectionInformation.m_mouseOverPoint)
+        {
+            m_selectionInformation.m_mouseOverLine = false;
+            m_selectionInformation.m_lineIndex = -1;
+        }
+        else
+        {
+            // Check to see if the mouse is near any shape line.
+            int mouseOverLineIndex = -1;
+            float closestLineDistance = m_gizmoDiscRadius;
+            for (int i = 0; i < m_scVolume.m_volumeShapes.Count; i++)
+            {
+                SCVolumeShape curShape = m_scVolume.m_volumeShapes[i];
+                for (int j = 0; j < curShape.m_boundingPoints.Count; j++)
+                {
+                    Vector3 curPoint = curShape.m_boundingPoints[j];
+                    Vector3 nextPont = curShape.m_boundingPoints[(j + 1) % curShape.m_boundingPoints.Count];
+                    float distanceFromMouseToLine = HandleUtility.DistancePointToLineSegment(mousePosition, curPoint, nextPont);
+                    if (distanceFromMouseToLine < closestLineDistance)
+                    {
+                        closestLineDistance = distanceFromMouseToLine;
+                        mouseOverLineIndex = j;
+                        mouseOverShapeIndex = i;
+                    }
+                }
+            }
+
+            // Update selected line information.
+            if(m_selectionInformation.m_lineIndex != mouseOverLineIndex || mouseOverShapeIndex != m_selectionInformation.m_mouseOverShapeIndex)
+            {
+                m_selectionInformation.m_mouseOverShapeIndex = mouseOverShapeIndex;
+                m_selectionInformation.m_lineIndex = mouseOverLineIndex;
+                m_selectionInformation.m_mouseOverLine = mouseOverLineIndex != -1;
+                m_needsRepaint = true;
+            }
+        }
+    }
+
+    private void SelectPointUnderMouse()
+    {
+        m_selectionInformation.m_pointSelected = true;
+        m_selectionInformation.m_mouseOverPoint = true;
+        m_selectionInformation.m_mouseOverLine = false;
+        m_selectionInformation.m_lineIndex = -1;
+
+        m_selectionInformation.m_dragStartPosition = SelectedShape.m_boundingPoints[m_selectionInformation.m_pointIndex];
+        m_needsRepaint = true;
+    }
+
+    private void SelectShapeUnderMouse()
+    {
+        if(m_selectionInformation.m_mouseOverShapeIndex != -1)
+        {
+            m_selectionInformation.m_shapeIndex = m_selectionInformation.m_mouseOverShapeIndex;
+            m_needsRepaint = true;
+        }
+    }
+
+    #endregion
+
+
+    #region Volume Management
+
+    private void CreateNewVolumeShape()
+    {
+        Undo.RecordObject(m_scVolume, "Create new VolumeShape");
+        m_scVolume.m_volumeShapes.Add(new SCVolumeShape());
+        m_selectionInformation.m_shapeIndex = m_scVolume.m_volumeShapes.Count - 1;
+    }
+
+    private void CreateNewVolumeShapePoint(Vector3 position)
+    {
+        bool mouseIsOverSelectedShape = m_selectionInformation.m_mouseOverShapeIndex == m_selectionInformation.m_shapeIndex;
+        int newPointIndex = m_selectionInformation.m_mouseOverLine && mouseIsOverSelectedShape ? m_selectionInformation.m_lineIndex + 1 : SelectedShape.m_boundingPoints.Count;
+        Undo.RecordObject(m_scVolume, "Add New Point");
+        SelectedShape.m_boundingPoints.Insert(newPointIndex, position);
+        m_selectionInformation.m_pointIndex = newPointIndex;
+        m_selectionInformation.m_mouseOverShapeIndex = m_selectionInformation.m_shapeIndex;
+        m_needsRepaint = true;
+
+        SelectPointUnderMouse();
+    }
+
+    #endregion
+
+
+    #region Rendering
+
+    private void DrawBounds()
+    {
+        for (int i = 0; i < m_scVolume.m_volumeShapes.Count; i++)
+        {
+            SCVolumeShape volumeShape = m_scVolume.m_volumeShapes[i];
+            bool shapeIsSelected = i == m_selectionInformation.m_shapeIndex;
+            bool mouseIsOverShape = i == m_selectionInformation.m_mouseOverShapeIndex;
+            Color deselectedShapeColour = Color.gray;
+
+            for (int j = 0; j < volumeShape.m_boundingPoints.Count; j++)
+            {
+                Vector3 curPoint = volumeShape.m_boundingPoints[j];
+                Vector3 nextPont = volumeShape.m_boundingPoints[(j + 1) % volumeShape.m_boundingPoints.Count];
+
+                if (j == m_selectionInformation.m_lineIndex && mouseIsOverShape)
+                {
+                    Handles.color = Color.cyan;
+                    Handles.DrawLine(curPoint, nextPont);
+                }
+                else
+                {
+                    Handles.color = shapeIsSelected ? Color.green : deselectedShapeColour;
+                    Handles.DrawDottedLine(curPoint, nextPont, 4);
+                }
+
+
+                if (j == m_selectionInformation.m_pointIndex && mouseIsOverShape)
+                {
+                    Handles.color = m_selectionInformation.m_pointSelected ? Color.blue : Color.cyan;
+                }
+                else
+                {
+                    Handles.color = shapeIsSelected ? Color.green : deselectedShapeColour;
+                }
+                Handles.DrawSolidDisc(curPoint, Vector3.back, m_gizmoDiscRadius);
+
+            }
+        }
+        m_needsRepaint = false;
+    }
+
+    #endregion
+}
---
+++ Generator Unity Project/Layered Ecosystem Generator/Assets/Ecosystem Generator/Scripts/Vegetation Generation/Space Colonisation/SCVolume.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SCVolumeShape
+{
+    public List<Vector3> m_boundingPoints = new List<Vector3>();
+}
+
+public class SCVolume : MonoBehaviour
+{
+    public List<SCVolumeShape> m_volumeShapes = new List<SCVolumeShape>();
+}
